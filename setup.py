2166b0088ec1fb0764280ee95ebf25d199bc30c5
Adnan Afzal <adnan01786@gmail.com>
2022-03-21 23:53:59 +0000

    updated version number to 3.x and author details for this fork

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyDictionary", # Replace with your own username
-    version="2.0.1",
-    author="geekpradd",
-    author_email="pradd@outlook.com",
+    version="3.0.0",
+    author="Adnan Afzal",
+    author_email="adnan01786@gmail.com",
     description="A real dictionary module for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
